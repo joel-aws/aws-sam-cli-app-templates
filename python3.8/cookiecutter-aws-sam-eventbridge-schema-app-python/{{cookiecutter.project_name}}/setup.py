a7204fb99e9522bb6a4d6755facc2b0dd3cb0f73
joel-aws <57411589+joel-aws@users.noreply.github.com>
2020-05-15 16:51:38 -0400

    Fix: correct setuptools params

--- python3.8/cookiecutter-aws-sam-eventbridge-schema-app-python/{{cookiecutter.project_name}}/setup.py
+++ python3.8/cookiecutter-aws-sam-eventbridge-schema-app-python/{{cookiecutter.project_name}}/setup.py
@@ -1,5 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 {% set schema_package = cookiecutter.AWS_Schema_root.split('.')[0] %}
 setup(
-    name="{{ schema_package }}", version="1.0", packages=["{{ cookiecutter.function_name }}/{{ schema_package }}"],
+    name="{{ schema_package }}", 
+    version="1.0",
+    package_dir={"":"{{ cookiecutter.function_name }}"},
+    packages=find_packages(where="{{ schema_package }}"),
 )
